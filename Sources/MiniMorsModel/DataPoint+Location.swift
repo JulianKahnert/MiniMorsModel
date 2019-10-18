a47d4c84a2b71308d302a34e8ad94164091b8051
Julian Kahnert <mail@juliankahnert.de>
2019-10-18 21:15:14 +0200

    add public init

--- Sources/MiniMorsModel/DataPoint+Location.swift
+++ Sources/MiniMorsModel/DataPoint+Location.swift
@@ -11,5 +11,10 @@ extension DataPoint {
     public struct Location: Codable, Hashable, Equatable {
         public let latitude: Double
         public let longitude: Double
+
+        public init(latitude: Double, longitude: Double) {
+            self.latitude = latitude
+            self.longitude = longitude
+        }
     }
 }
